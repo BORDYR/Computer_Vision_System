bf8633f430410072196dd7632331df85c4cd8d42
Misha Kirillov <mishakirill@gmail.com>
2019-12-27 00:40:28 +0300

    Add changes

--- VideoSream_try.py
+++ VideoSream_try.py
@@ -1,67 +1,113 @@
 # For more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import cv2 as cv2
+import circlerecognizer
+
 import cv2 as cv
 import numpy as np
 import os
-import circlerecognizer
-
-FILE_OUTPUT = 'output.avi'
-image = cv2.imread('D:/PycharmProjects/object_detector_app/utils/j3.jpg')
-
-# Checks and deletes the output file
-# You cant have a existing file or it will through an error
-if os.path.isfile(FILE_OUTPUT):
-	os.remove(FILE_OUTPUT)
-
-# Playing video from file:
-# cap = cv2.VideoCapture('vtest.avi')
-# Capturing video from webcam:
-cap = cv2.VideoCapture(0)
-
-currentFrame = 0
+import Rectanglerecognizer
+import Rect_recoginzebycv
+import time
 
 
-width = cap.set(cv2.CAP_PROP_FRAME_WIDTH, 460);
-height  = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320);
 
 
-# Get current width of frame
-# width = cap.get(cv2.CV_CAP_PROP_FRAME_WIDTH)   # float
-# Get current height of frame
-# height = cap.get(cv2.CV_CAP_PROP_FRAME_HEIGHT) # float
+def start_recognition(iterations = 0):
+	cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+	if iterations:
+		for _ in range(iterations):
+			if (cap.isOpened()):
+				# Capture frame-by-frame
+				ret, frame = cap.read()
+				#print('Got the frame')
+				if ret == True:
+					# frame = cv2.flip(frame,1)
 
+					# Display the resulting frame
+					circle_rating, outer = circlerecognizer.circle_recognise(frame) # распознавание кругов
+					# outer = Rect_recoginzebycv.rec1(frame)			  # распознавание прямоуг от minAreaReact
+					# outer = Rectanglerecognizer.get_image_with_realogram(frame)
+					cv2.imshow('frame', outer)
 
-# Define the codec and create VideoWriter object
-# fourcc = cv2.CV_FOURCC(*'X264')
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(FILE_OUTPUT,fourcc, 20.0, (int(width),int(height)))
-
-# while(True):
-while(cap.isOpened()):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
-	print('Got the frame')
-	if ret == True:
-		frame = cv2.flip(frame,1)
-
-		# Saves for video
-		out.write(frame)
-
-		# Display the resulting frame
-		outer = circlerecognizer.circle_recognise(frame)
-		cv2.imshow('frame', outer)
-		print('See the circles?')
+				# time.sleep(0.5)
+				else:
+					break
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
 	else:
-		break
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-	currentFrame += 1
-	print(currentFrame)
-	# To stop duplicate images
+		while (cap.isOpened()):
+			# Capture frame-by-frame
+			ret, frame = cap.read()
+			#print('Got the frame')
+			if ret == True:
+				t = time.time()
+				frame = cv2.flip(frame, 1)
+
+				# Display the resulting frame
+				circle_rating, outer = circlerecognizer.circle_recognise(frame)  # распознавание кругов
+				#outer = Rect_recoginzebycv.rec1(frame)			  # распознавание прямоуг от minAreaReact
+				# outer = Rectanglerecognizer.get_image_with_realogram(frame)
+				cv2.imshow('frame', outer)
+
+			# time.sleep(0.5)
+			else:
+				break
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+	cap.release()
+	return circle_rating
+
+
+def get_defined_circles(circles_rating, n: int):
+	defined_circles = []
+	#print(circles_rating)
+	#print(circles_rating)
+	for k, v in circles_rating.items():
+		if v > n/2:
+			defined_circles.append(k.split(', ')[:2:])
+	print(f'Finally there are {len(defined_circles)} objects:')
+	print(defined_circles)
+	#print(get_objects_in_right_order(defined_circles))
+	return defined_circles
+
+def get_objects_in_right_order(defined_circles: list):
+	if len(defined_circles) == 1:
+		return defined_circles
+	else:
+		for i in range(len(defined_circles)-1):
+			if defined_circles[i][1] > defined_circles[i+1][1]:  # Changed < to >
+				defined_circles[i], defined_circles[i+1] = defined_circles[i+1], defined_circles[i]
+		return defined_circles
+
+
+
+def convert_to_absolute_coorditates(coord_list):
+	pass
+
+
+def send_coordinates_to_control_system(pipe_name='PipesOfPiece', data: str = 'test_message'):
+	if not pipe_name:
+		pipe_name = r'PipesOfPiece'
+	pipe_path = r'\\.\pipe\PipesOfPiece' #TODO: can't concatenate '\\.\pipe\PipesOfPiece' and 'PipesOfPiece'
+	message = data.encode('utf-8')
+	with open(pipe_path, mode='r+b', buffering=0) as f:
+		f.write(message)
+		print('message sent')
+		time.sleep(1)
+
+
+
+
+if __name__ == "__main__":
+	t = time.time()
+	scan_frames_number: int = 100 # n is times of circle scan
+	circles = start_recognition(scan_frames_number)
+	#def_circles = get_defined_cirles(circles)
+	get_defined_circles(circles, scan_frames_number)
+	print(time.time() - t)
+	#send_coordinates_to_control_system()
+
+	cv2.destroyAllWindows()
 
 
 # When everything done, release the capture
-cap.release()
-out.release()
-cv2.destroyAllWindows()
--- circlerecognizer.py
+++ circlerecognizer.py
@@ -1,41 +1,49 @@
 import numpy as np
+import collections
+import operator
 import argparse
 import cv2
+import time
 
 image = cv2.imread('D:/PycharmProjects/object_detector_app/utils/j3.jpg')
+circles_rating = collections.defaultdict(int)
 
 def viewImage(image, window : str):
     cv2.namedWindow(window, cv2.WINDOW_NORMAL)
     cv2.imshow(window, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-def circle_recognise(image):
 
-	#viewImage(image)
-	# load the image, clone it for output, and then convert it to grayscale
-	#image = cv2.imread(args["image"])
+def circle_recognise(image):
 	output = image.copy()
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	# detect circles in the image
-	circles = cv2.HoughCircles(image=gray, method=cv2.HOUGH_GRADIENT, dp=2, minDist=2000, minRadius=50, maxRadius=200)
-	height, width, channels = image.shape
-	print('height = {}, width = {}'.format(height, width))
+	circles = get_circles(image)
+
 	# ensure at least some circles were found
-	if circles is not None:
+	if (circles is not None) :
 		# convert the (x, y) coordinates and radius of the circles to integers
 		circles = np.round(circles[0, :]).astype("int")
-
 		# loop over the (x, y) coordinates and radius of the circles
+
 		for (x, y, r) in circles:
-			cv2.circle(output, (x, y), r, (0,255,0), 4)
-			print('Circle has been detected! x = {}p, y = {}p, r = {}p'.format(x, y, r))
-			#viewImage(output, 'frame')
-			#cv2.imshow('ff', output)
+			x, y, r = x.item(), y.item(), r.item()
+			cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+			print(f'Circle has been detected! x = {x}p, y = {y}p, r = {r}p')
+			cir = f'{x}, {y}, {r}'
+			circles_rating[cir] += 1
 	else:
 		print('Circles haven\'t been found')
-	return output
-#circle_recognise(image)
-		# show the output image
+	return dict(circles_rating), output
 
-		#cv2.imshow("output", np.hstack([image, output]))
-		#cv2.waitKey(0)
+
+def get_circles(image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# Detect circles in the image
+	# Actual circle size is 25mm.
+	#cv2.GaussianBlur(image, gray, ksize=)
+	#gray = cv2.blur(gray, (2, 2))
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	circles = cv2.HoughCircles(image=gray, method=cv2.HOUGH_GRADIENT, dp=2, minDist=22, minRadius=25, maxRadius=32)
+
+	height, width, channels = image.shape
+	#print('height = {}, width = {}'.format(height, width))
+	return circles
